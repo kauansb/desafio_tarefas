71d4388dd20194e39630201bcf5dab7fab896924
Kauan Santana Barros <kauansbflu@gmail.com>
2024-10-08 20:17:09 -0300

    feat
    add página home

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from app.views import HomeView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('', HomeView.as_view(), name='home'),
 
     path('tarefas/', include('tarefas.urls')), 
 
