e3b48f822222ad8e4fc0d83886e15cc7f7744564
Kauan Santana Barros <kauansbflu@gmail.com>
2024-10-08 22:14:43 -0300

    style
    add static

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from app.views import HomeView
 
@@ -13,3 +15,6 @@ urlpatterns = [
 
     path('registros/', include('registros.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
